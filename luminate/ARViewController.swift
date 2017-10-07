2a419e7bdfdb1792773b7ec5e189a87fb5523895
Prayash Thapa <prayasht@gmail.com>
2017-10-07 11:14:41 -0400

    Add geometry generation subroutine. Add a Float extension for randoms.

--- luminate/ARViewController.swift
+++ luminate/ARViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
-import SceneKit
 import ARKit
+import SceneKit
 
 class ARViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    var geometries = [SCNSphere(radius: 0.05),
+                      SCNPlane(width: 0.05, height: 0.05),
+                      SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0.0025),
+                      SCNPyramid(width: 0.01, height: 0.01, length: 0.01),
+                      SCNBox(width: 0.25, height: 0.1, length: 0.1, chamferRadius: 0.0),
+                      SCNPyramid(width: 0.05, height: 0.05, length: 0.05)]
     
     // MARK: - View Controller Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScene()
+        setupRecognizers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,11 +39,6 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
     
     // MARK: - Setup Methods
@@ -72,6 +75,65 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    func setupRecognizers() {
+        // Single taps insert a new geometry into scene
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapFrom))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // MARK: - Tap Handling Callbacks
+    @objc func handleTapFrom(recognizer: UITapGestureRecognizer) {
+        generateParticles()
+    }
+    
+    func generateParticles() {
+        print("Generating particles...")
+
+        self.sceneView.scene.physicsWorld.gravity = SCNVector3Make(0.0, 0.0, 0.0)
+        
+        var angle: Float = 0.0
+        let angleInc: Float = Float.pi / Float(geometries.count)
+        
+        for i in 0 ..< geometries.count {
+            let radius: Float = Float.random(min: 0.5, max: 2.5)
+            let phi: Float = Float.random(min: 0.5, max: 5)
+            let theta: Float = Float.random(min: 0.5, max: 5)
+ 
+            let parentOrb = SCNSphere(radius: CGFloat(Float.random(min: 0.05, max: 0.08)))
+            parentOrb.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: CGFloat(Float.random(min: 0.4, max: 0.6)))
+            
+            let childOrb = SCNSphere(radius: CGFloat(Float.random(min: 0.03, max: 0.04)))
+            childOrb.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 1.0)
+            childOrb.firstMaterial?.emission.contents = UIColor(white: 1.0, alpha: 1.0)
+            
+            
+            let node = SCNNode(geometry: parentOrb)
+            let child = SCNNode(geometry: childOrb)
+            node.addChildNode(child)
+            
+            
+            let displacement: Float = 0.025
+            let up = SCNAction.moveBy(x: 0.0, y: CGFloat(displacement), z: 0.0, duration: 5.0)
+            let down = SCNAction.moveBy(x: 0.0, y: CGFloat(-displacement), z: 0.0, duration: 5.0)
+            
+            let oscillation = SCNAction.repeatForever(SCNAction.sequence([up, down]))
+            let rotation = SCNAction.repeatForever(SCNAction.rotateBy(x: 1, y: 1, z: 1, duration: 10))
+            let actions = SCNAction.group([oscillation, rotation])
+            
+            node.runAction(actions)
+            node.scale = SCNVector3Make(0.5, 0.5, 0.5)
+            node.position = SCNVector3Make(
+                radius * sin(theta) * cos(phi),
+                radius * sin(theta) * sin(phi),
+                radius * cos(theta)
+            )
+            
+            self.sceneView.scene.rootNode.addChildNode(node)
+            angle += angleInc
+        }
     }
     
     // MARK: - Renderer Delegate Methods
---
+++ luminate/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  luminate
+//
+//  Created by Prayash Thapa on 10/7/17.
+//  Copyright © 2017 com.reality.af. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Float Extension
+
+public extension Float {
+    
+    public static var random: Float {
+        return Float(arc4random()) / 0xFFFFFFFF
+    }
+    
+    public static func random(min: Float, max: Float) -> Float {
+        return Float.random * (max - min) + min
+    }
+}
