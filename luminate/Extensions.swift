3488084b0409504edddaa425abf303e679dbea4a
Jesse Litton <jesse.litton@gmail.com>
2017-10-08 14:10:10 -0400

    Adds some UIView extensions... sets up transition class for animating.

--- luminate/Extensions.swift
+++ luminate/Extensions.swift
@@ -5,7 +5,7 @@
 //  Created by Prayash Thapa on 10/7/17.
 //  Copyright © 2017 com.reality.af. All rights reserved.
 //
-
+import UIKit
 import Foundation
 
 // MARK: Float Extension
@@ -18,5 +18,18 @@ public extension Float {
     
     public static func random(min: Float, max: Float) -> Float {
         return Float.random * (max - min) + min
+    }
+}
+extension UIView {
+    func fadeIn() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    func fadeOut() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.alpha = 0.0
+        }, completion: nil)
     }
 }
